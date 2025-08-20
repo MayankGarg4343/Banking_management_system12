0eb2db7a3ed09f319c84e9ddaeadea7ee04945ff
MayankGarg4343 <mgsm4343@gmail.com>
2025-08-20 21:01:09 +0530

    first commit

---
+++ src/bank/management/system/Login.java
@@ -0,0 +1,13 @@
+package bank.management.system;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    Login(){ //constructor of the class.
+        setSize(850,480); // this functions sets the width and size of the frame.
+        setVisible(true); 
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
